841135d7c76684ec953defe5ec55c8da4a6ef979
Luan <lujetix@gmail.com>
2022-07-06 13:04:02 -0300

    change name package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='robotpy',
     version='0.1.1',
-    packages=find_packages(include=['robotpy', 'robotpy.*']),
+    packages=['robotpy'],
 )
