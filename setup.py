cb4c06eea61263051306e189c973907ae97a367f
Luan Grabher <luanggcontato@gmail.com>
2022-07-06 12:26:42 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ version = "0.0.1"
 description = "Robot library to start calls and return messages to database"
 
 [options]
-packages = find:
+packages = "find:"
 install_requires =
     requests
     python_version<"3.8"
