852ea68d1acf775854de647f23db9014c0088f28
Luan Grabher <luanggcontato@gmail.com>
2022-07-06 12:31:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-[metadata]
-name = robotpy
-version = "0.0.1"
-description = "Robot library to start calls and return messages to database"
+metadata = [
+  'name' = robotpy
+  'version' = "0.0.1"
+  'description' = "Robot library to start calls and return messages to database"
+]
 
-[options]
-packages = "find:"
-install_requires = ["requests", 'python_version<"3.8"']
+options = [
+  'packages' = "find:"
+  'install_requires' = ["requests", 'python_version<"3.8"']
+]
