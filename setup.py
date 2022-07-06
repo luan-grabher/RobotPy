615a71c20a6db211487dd102c0fafa3f4ec2c9b5
Luan <lujetix@gmail.com>
2022-07-06 12:37:16 -0300

    test setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
-metadata = {
-  'name': "robotpy",
-  'version': "0.0.1",
-  'description': "Robot library to start calls and return messages to database"
-}
+from setuptools import setup, find_packages
 
-options = {
-  'packages': "find:",
-  'install_requires': ["requests", 'python_version<"3.8"']
-}
+setup(
+    name='robotpy',
+    version='0.1.0',
+    packages=find_packages(include=['*'])
+)
